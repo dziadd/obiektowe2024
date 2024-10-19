af9994a04b0e73937ead24e3b7eec96ad0d0affc
Krystian <147257891+dziadd@users.noreply.github.com>
2024-10-19 17:01:51 +0200

    14 i 15 zst3

--- Main.java
+++ Main.java
@@ -1,133 +1,46 @@
-import java.sql.Date;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.Scanner;
 
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-
-        //1
-        System.out.println("Krystian Dobrołęcki");
-
-        //2
-        String im = "Krystian Dobrołęcki";
-        System.out.println(im.length());
-
-        //3
-        String a = "Ala ";
-        String b = "ma ";
-        String c = "kota";
-        String d = a+b+c;
-
-        System.out.println(d);
-
-        //4
-
-        System.out.println("                    *");
-        System.out.println("                    * *");
-        System.out.println("                    * * *");
-        System.out.println("* * * * * * * * * * * * * *");
-        System.out.println("* * * * * * * * * * * * * * *");
-        System.out.println("* * * * * * * * * * * * * *");
-        System.out.println("                    * * *");
-        System.out.println("                    * *");
-        System.out.println("                    *");
-
-        //5
-
-        String txt = "Ala ma kota";
-        String txt1 = txt.replace("a","e");
-        System.out.println(txt1);
-
-        //6
-
-        System.out.println(txt.toUpperCase());
-        System.out.println(txt.toLowerCase());
-
-        //7
-
-        int ascii = 'A';
-        int ascii1 = '!';
-        int ascii2 = '@';
-        int ascii3 = '>';
-        int ascii4 = '~';
-        int ascii5 = '\n';
-        int ascii6 = '\b';
-
-
-        System.out.println(ascii);
-        System.out.println(ascii1);
-        System.out.println(ascii2);
-        System.out.println(ascii3);
-        System.out.println(ascii4);
-        System.out.println(ascii5);
-        System.out.println(ascii6);
-
-        //8
-
-        System.out.println("Duże litery w ASCII od 65-90, małe od 97-122, cyfry 48-57");
-
-        //9
-
-        String zdanie = "Grzesiek nie wiedział dlaczego jest tak drapieżnym %s mimo,że jego %s na to nie wskazuje.";
-        String zzdanie = String.format(zdanie,"knurem", "waga");
-        System.out.println(zzdanie);
-
-        //10
-        String ksiazka = "%s to najlepsza książka napisana przez %s";
-        String ksiazkas = String.format(ksiazka,"Wiedźmin Ostatnie Życzenie", "Andrzeja Sapkowskiego.");
-        System.out.println(ksiazkas);
-
-        //11
-
-        String w = "wodrze ";
-        System.out.println(w.repeat(5));
-
-        //12
-
-        LocalDate data = LocalDate.now();
-        LocalTime czas = LocalTime.now();
+class zadania {
+            public static boolean palindrom(String s) {
+                for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+                    if (s.charAt(i) != s.charAt(j)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
 
 
 
-        System.out.println("Dzisiaj jest " + data + " godzina " + czas);
+            public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+//                System.out.println("Podaj liczbę, by sprawdzić czy jest palindromem?");
+//                String liczba = scan.nextLine();
+//                boolean b1 = palindrom(liczba);
+//                System.out.println(liczba + " " + b1);
 
-        //13
 
-        char q = 54;
+                System.out.println("Podaj wysokość trójkąta pascala:");
+                int n = scan.nextInt();
+
+                for (int l = 0; l < n; l++) {
+                    for(int spc = 0; l < n; l++){
+                        System.out.print(" ");
+                    }
+                    int one = 1;
+                    for (int i = 0; i<=l; i++) {
+                        System.out.print(one + " ");
+                        one = one * (l - i) / (i + 1);
+                    }
+                    System.out.println();
+                }
 
 
 
 
 
 
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
 }
+
