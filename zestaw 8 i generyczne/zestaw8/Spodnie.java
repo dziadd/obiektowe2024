42abf009aa0eee7415219cdd1c7d1e6acd5ea650
Krystian <147257891+dziadd@users.noreply.github.com>
2025-01-18 22:32:28 +0100

    Add files via upload

---
+++ zestaw 8 i generyczne/zestaw8/Spodnie.java
@@ -0,0 +1,29 @@
+public class Spodnie extends Ubranie{
+public Spodnie(String nazwa, double cena, int ilosc){
+    super(nazwa, cena, ilosc);
+}
+    @Override
+    public void wypierz() {
+
+    }
+
+    @Override
+    public void zaloz() {
+
+    }
+
+    @Override
+    public void wyprasuj() {
+
+    }
+
+    @Override
+    public void wysusz() {
+
+    }
+
+    @Override
+    public void zniszcz() {
+
+    }
+}
